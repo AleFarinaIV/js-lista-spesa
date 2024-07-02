59751a4eba3755f95be8ee1f655d453f62a41145
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-07-02 16:34:28 +0200

    [UPD] created var count = 0 and init while cycle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 const ingredients = ['pomodori', 'pasta', 'guanciale', 'pepe nero', 'pesto', 'macinato']
 
 const shoppingList = document.getElementById('shopping-list')
+
+let count = 0
+
+while (count < ingredients.length) {
+    let item = document.createElement('li')
+}
